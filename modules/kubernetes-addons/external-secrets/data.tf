c318cd2aff45597cadaae25e1e0b3afd5e0bb773
florentio <desouza.florentio@gmail.com>
2022-10-06 06:47:12 -0400

    feat: test

--- modules/kubernetes-addons/external-secrets/data.tf
+++ modules/kubernetes-addons/external-secrets/data.tf
@@ -1,16 +1,22 @@
 data "aws_iam_policy_document" "external_secrets" {
-  statement {
-    actions   = ["ssm:GetParameter"]
-    resources = var.external_secrets_ssm_parameter_arns
+  dynamic "statement" {
+    for_each = length(var.external_secrets_ssm_parameter_arns) > 0 ? { enabled : true } : {}
+    content {
+      actions   = ["ssm:GetParameter"]
+      resources = var.external_secrets_ssm_parameter_arns
+    }
   }
 
-  statement {
-    actions = [
-      "secretsmanager:GetResourcePolicy",
-      "secretsmanager:GetSecretValue",
-      "secretsmanager:DescribeSecret",
-      "secretsmanager:ListSecretVersionIds",
-    ]
-    resources = var.external_secrets_secrets_manager_arns
+  dynamic "statement" {
+    for_each = length(var.external_secrets_secrets_manager_arns) > 0 ? { enabled : true } : {}
+    content {
+      actions = [
+        "secretsmanager:GetResourcePolicy",
+        "secretsmanager:GetSecretValue",
+        "secretsmanager:DescribeSecret",
+        "secretsmanager:ListSecretVersionIds",
+      ]
+      resources = var.external_secrets_secrets_manager_arns
+    }
   }
 }
